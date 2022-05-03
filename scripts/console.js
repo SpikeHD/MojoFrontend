cfe868f49892b0cafee6b8258657a9c1db224395
SpikeHD <spikegdofficial@gmail.com>
2022-05-03 02:40:07 -0700

    more scaffolding

---
+++ scripts/console.js
@@ -0,0 +1,23 @@
+function sendCommand(payload){
+  let client = new XMLHttpRequest();
+  let key = new window.URLSearchParams(window.location.search).get("k");
+  let url = '/mojoplus/api';
+
+  client.open("POST", url, true);
+  client.setRequestHeader("Content-Type", "application/json");
+
+  client.onreadystatechange = () => {
+    if (client.readyState === 4 && client.status === 200) {
+      let result = document.getElementById("c2");
+
+      // Print received data from server
+      result.innerHTML = JSON.parse(this.responseText).payload.replace(/\n/g, "<p/>");
+    }
+  };
+
+  // Converting JSON data to string
+  var data = JSON.stringify({ "k": key, "request": "invoke", "payload": payload });
+  
+  // Sending data with the request
+  client.send(data);
+}
