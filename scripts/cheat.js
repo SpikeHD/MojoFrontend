963ad3e7f53acb95b0d2b81791a971f14bb4d67e
mingjun97 <my@lyric.today>
2022-05-04 19:01:01 -0700

    Refactor and imporvement to the cheating page

--- scripts/cheat.js
+++ scripts/cheat.js
@@ -1,43 +1,13 @@
-qucick_command = [
-    {name: "Heal All Characters", command: "heal", args: []},
-    {name: "Give 1000000 Mora", command: "give 202 1000000", args: []},
-    {name: "Give 100000 Proigem", command: "give 201 100000", args: []},
-    {name: "God mode", command: "godmode", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items", command: "clear all", args: []},
-    {name: "Clear all items111", command: "clear all", args: []},
-]
 
 document.addEventListener("DOMContentLoaded", ()=> {
+    var toolset = document.getElementsByClassName("toolset")[0];
     var panel = document.getElementById("panel");
-    var i = 0;
-    for (i=0; i< qucick_command.length; i++) {
-        var command = qucick_command[i];
-        console.log(i);
-        var div = document.createElement("div");
-        var label = document.createElement("span");
-        var button = document.createElement("button");
-        var hr = document.createElement("hr");
-        hr.classList.add("solid");
-        div.classList.add('commandGroup');
-        label.innerText = command.name;
-        button.innerText = "Go!";
-        button.command_id = i;
-        div.appendChild(label);
-        div.appendChild(button);
-        panel.appendChild(div);
-        button.onclick = (e) => {
-            var cmd = qucick_command[e.target.command_id];
-            var payload = cmd.command;
-            sendCommand(payload);
+    toolset.onclick = (e)=> {
+        if (e.target != toolset && e.target.tagName == "LABEL") {
+            panel.innerHTML = ""; // clear the panel
         }
-        panel.appendChild(hr);
+        console.log(e.target);
     }
+   genQuickCommand();
+   document.getElementById("tab-1").onclick = genQuickCommand;
 });
---
+++ scripts/cheat/quickcommand.js
@@ -0,0 +1,78 @@
+qucick_command = [
+    {name: "Heal All Characters", command: "heal", args: []},
+    {name: "Give Mora", command: "give 202", args: [
+        {type: "number", default: 100000, width: 145}
+    ]},
+    {name: "Give Proigem", command: "give 201", args: [
+        {type: "number", default: 10000, width: 120}
+    ]},
+    {name: "Give Intertwined Fate", command: "give 223", args: [
+        {type: "number", default: 10000, width: 100}
+    ]},
+    {name: "Give Acquaint Fate", command: "give 224", args: [
+        {type: "number", default: 10000, width: 100}
+    ]},
+    {name: "Toggle Godmode", command: "godmode", args: []},
+    {name: "Set world level(relog required)", command: "setworldlevel", args: [
+        {type: "number", default: 8, width: 60}
+    ]}, 
+    {name: "Give all items", command: "giveall", args: []},
+    {name: "Clear all items", command: "clear all", args: []},
+]
+
+function genQuickCommand() {
+    var i = 0;
+    var arg = 0;
+    for (i=0; i< qucick_command.length; i++) {
+        var command = qucick_command[i];
+        var div = document.createElement("div");
+        var label = document.createElement("span");
+        var div2 = document.createElement("div");
+        var button = document.createElement("button");
+        var hr = document.createElement("hr");
+        var hiddenCommand = document.createElement("input");
+
+        hr.classList.add("solid");
+        div.classList.add('commandGroup');
+        label.innerText = command.name;
+        button.innerText = "Go!";
+        div.appendChild(label);
+        div.appendChild(div2);
+
+        hiddenCommand.value = command.command;
+        hiddenCommand.classList.add('hidden');
+        div2.appendChild(hiddenCommand);
+        for (arg = 0; arg < command.args.length; arg++){
+            var arg_item = command.args[arg];
+            switch (arg_item.type) {
+                case "number": 
+                    var input = document.createElement('input');
+                    input.type = arg_item.type;
+                    input.value = arg_item.default;
+                    if (arg_item.width) {
+                        input.style.width = arg_item.width + 'px';
+                    }
+                    div2.appendChild(input);
+            }
+        }
+
+
+        div2.appendChild(button);
+        panel.appendChild(div);
+        button.onclick = (e) => {
+            var parent = e.target.parentNode;
+            var payload = "";
+            var first = true;
+            for(var child=parent.firstChild; child!==null; child=child.nextSibling) {
+                if (child.tagName != "INPUT") continue;
+                if (!first) {
+                    payload += " ";
+                }
+                first = false;
+                payload += child.value;
+            }
+            sendCommand(payload);
+        }
+        panel.appendChild(hr);
+    }
+}
